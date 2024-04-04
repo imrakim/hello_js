418d8d1ac0a88f035e1864b6cf2b8d4688d5dc35
Rakim Ali <rakimali770@gmail.com>
2024-04-05 01:28:16 +0600

    hello js

---
+++ hello.js
@@ -0,0 +1,10 @@
+const br ="hello";
+// const declerable but not updateable there everything fixed
+var hello="i'm Rakim";
+hello="30";
+// var is not use able now that is main topic
+
+
+
+// we are using that
+let 
