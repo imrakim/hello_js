6f3f7382e5faa070485d1c7143e8ce9d239ac08d
Rakim Ali <rakimali770@gmail.com>
2024-04-07 12:35:23 +0600

    variables and numbers learning

--- hello.js
+++ hello.js
@@ -1,10 +1,27 @@
-const br ="hello";
-// const declerable but not updateable there everything fixed
-var hello="i'm Rakim";
-hello="30";
-// var is not use able now that is main topic
+var jsHome = "hello brother";
+jsHome = "6+5";
+jsHome = "its your code not My ";
+jsHome = "It Dosen't matter Who I'm ";
+jsHome = "I'm Alone But happy ";
+jsHome = "are you want to say anything";
+console.log(jsHome)
+
+let a = "Hey there My Name is Rakim a Front end Developer";
+a = " I want be a front end Developer"
+console.log(a)
+
+let b = 10;
+let c = 5;
+let d = 5;
+
+console.log("b-c =", b - c);
+console.log("d*c =", d * c);
+console.log("b/c =",  b / c);
+console.log("b+c =", b + c)
 
 
+string  e  qutations add kora jabe but  numbers e qutaions add  kora jabe nah   jdi add kori tahole ans vul asbe but kono error asbe nah !  
 
-// we are using that
-let 
+string = "";
+number = ;
+
