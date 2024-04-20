fa1d6133855e5a4de6e15e082f28eefb9a77d279
Rakim Ali <rakimali770@gmail.com>
2024-04-20 14:30:01 +0600

    learning modulas

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ jsHome = "I'm Alone But happy ";
 jsHome = "are you want to say anything";
 console.log(jsHome)
 
-let a = "Hey there My Name is Rakim a Front end Developer";
+let a = "Hey there I'm Rakim a Front end Developer";
 a = " I want be a front end Developer"
 console.log(a)
 
@@ -54,6 +54,22 @@ let pnn = 80;
 
 let pzzz = pmm + pnn;
 console.log("pmm + pnn =",pzzz);
+
+let rrr = 40;
+let rr = 45;
+let rrrrr = rrr + rr;
+console.log("rrr+rr =",rrrrr);
+
+
+let space = "23" + 5 +3;
+console.log("33+5+3=",space);
+let mahin = 6+2 +"8";
+console.log(mahin);
+
+let r = 50;
+let s = 20;
+ let rs = 50 % 20;
+ console.log(rs);
 
 
 
