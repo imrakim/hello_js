88662303873ef4c2340992cf83e1cdbc5c20cc1e
Rakim Ali <rakimali770@gmail.com>
2024-04-23 23:55:49 +0600

    learning operator

--- hello.js
+++ hello.js
@@ -1,109 +1,132 @@
-var jsHome = "hello brother";
-jsHome = "6+5";
-jsHome = "its your code not My ";
-jsHome = "It Dosen't matter Who I'm ";
-jsHome = "I'm Alone But happy ";
-jsHome = "are you want to say anything";
-console.log(jsHome)
+// var jsHome = "hello brother";
+// jsHome = "6+5";
+// jsHome = "its your code not My ";
+// jsHome = "It Dosen't matter Who I'm ";
+// jsHome = "I'm Alone But happy ";
+// jsHome = "are you want to say anything";
+// console.log(jsHome)
 
-let a = "Hey there I'm Rakim a Front end Developer";
-a = " I want be a front end Developer"
-console.log(a)
+// let a = "Hey there I'm Rakim a Front end Developer";
+// a = " I want be a front end Developer"
+// console.log(a)
 
-let b = 10;
-let c = 5;
-let d = 5;
-let x = b + c;
+// let b = 10;
+// let c = 5;
+// let d = 5;
+// let x = b + c;
 
-console.log("b =",b , "c =",c)
-console.log("b - c =", b - c);
-console.log("d * c =", d * c);
-console.log("b / c =",  b / c);
-console.log("b + c =", b + c)
-console.log("b + c =", x)
+// console.log("b =",b , "c =",c)
+// console.log("b - c =", b - c);
+// console.log("d * c =", d * c);
+// console.log("b / c =",  b / c);
+// console.log("b + c =", b + c)
+// console.log("b + c =", x)
 
-let myName = 24;
-myName = 456867597597;
-myName = 1935901104;
+// let myName = 24;
+// myName = 456867597597;
+// myName = 1935901104;
 
-console.log(typeof (myName));
+// console.log(typeof (myName));
 
-let rakim = "I'm Proud of You";
-var m = "Bangladesh";
-{
-m = "natore"
-console.log(m);
-}
-m = "I'm Rakim";
-console.log(m);
-let mr = "hlw guys";
-const md ="hgifbgsnbv";
+// let rakim = "I'm Proud of You";
+// var m = "Bangladesh";
 // {
-//     const p = "gh"; 
+// m = "natore"
+// console.log(m);
 // }
-// console.log(p);
+// m = "I'm Rakim";
+// console.log(m);
+// let mr = "hlw guys";
+// const md ="hgifbgsnbv";
+// // {
+// //     const p = "gh"; 
+// // }
+// // console.log(p);
 
 
 
-// string  e  qutations add kora jabe but  numbers e qutaions add  kora jabe nah   jdi add kori tahole ans vul asbe but kono error asbe nah !  
+// // string  e  qutations add kora jabe but  numbers e qutaions add  kora jabe nah   jdi add kori tahole ans vul asbe but kono error asbe nah !  
 
-// string = "";
-// number = ;
-
-
-
-
-//   arithmetic operators end 
-let pmm = 90;
-let pnn = 80;
-
-let pzzz = pmm + pnn;
-console.log("pmm + pnn =",pzzz);
-
-let rrr = 40;
-let rr = 45;
-let rrrrr = rrr + rr;
-console.log("rrr+rr =",rrrrr);
-
-
-let space = "23" + 5 +3;
-console.log("33+5+3=",space);
-let mahin = 6+2 +"8";
-console.log(mahin);
-
-let r = 50;
-let s = 20;
- let rs = 50 % 20;
- console.log(rs);
-  let mm = 10;
-  mm ++;
-  ++mm
-  console.log(mm);
-  let nns = 33;
-  ++nns;
-  --nns;
-  nns++
-  nns--
-  console.log(nns);
-
-
-
-// precedent operator start learning
-
-let ab = 2;
-let bc = 2;
-let cb = 12;
-let abc = ab + bc ** cb;
-console.log(abc);
-
-
-let rsss = null;
-console.log(typeof rsss);
+// // string = "";
+// // number = ;
 
 
 
 
-// what is datatypes
+// //   arithmetic operators end 
+// let pmm = 90;
+// let pnn = 80;
+
+// let pzzz = pmm + pnn;
+// console.log("pmm + pnn =",pzzz);
+
+// let rrr = 40;
+// let rr = 45;
+// let rrrrr = rrr + rr;
+// console.log("rrr+rr =",rrrrr);
+
+
+// let space = "23" + 5 +3;
+// console.log("33+5+3=",space);
+// let mahin = 6+2 +"8";
+// console.log(mahin);
+
+// let r = 50;
+// let s = 20;
+//  let rs = 50 % 20;
+//  console.log(rs);
+//   let mm = 10;
+//   mm ++;
+//   ++mm
+//   console.log(mm);
+//   let nns = 33;
+//   ++nns;
+//   --nns;
+//   nns++
+//   nns--
+//   console.log(nns);
 
 
 
+// // precedent operator start learning
+
+// let ab = 2;
+// let bc = 2;
+// let cb = 12;
+// let abc = ab + bc ** cb;
+// console.log(abc);
+
+
+// let rsss = null;
+// console.log(typeof rsss);
+
+
+
+
+// // what is datatypes
+
+let number = 5;
+let anotherNumber= 10;
+// console.log(number);
+// number = number+1;
+// console.log(number);
+// number++
+// anotherNumber=anotherNumber+number++
+// console.log(anotherNumber,number);
+anotherNumber=anotherNumber+ ++number
+console.log(anotherNumber,number);
+let numberOne = "10";
+let numberTwo ="2";
+let stringOne="ten";
+let stringTwo="two";
+
+// hint what is overloaded operator in javascript?
+
+
+//homework apply arithmethic operator
+// pre and post niye 2 tah example
+//
+
+
+// ++number
+// console.log(number);
