e36fcddb10a45be3dbb59fba9ab2b7de0580ef83
Rakim Ali <rakimali770@gmail.com>
2024-04-26 00:20:15 +0600

    arrary convert string

--- hello.js
+++ hello.js
@@ -240,6 +240,7 @@ console.log(rakim[3][1][2]);
 
 const mouse = [ 23, 24, 25, "nadim","samim",[ false, true,26,["rakim",["the end"]]]]
 console.log(mouse [5][3]);
+console.log(mouse.toString());
 
 //how to define an array | Indexed array
 // array literal
